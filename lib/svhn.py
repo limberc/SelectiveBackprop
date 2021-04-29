5f731de3c406b913d7c0366beda8752bfbb2692d
Zehua Cheng <chengzehua@outlook.com>
2021-04-29 23:54:59 +0800

    Delete unused module.

--- lib/svhn.py
+++
@@ -1,7 +0,0 @@
-from torchvision import datasets
-
-
-class SVHN(datasets.SVHN):
-    def __getitem__(self, index):
-        retval = super(SVHN, self).__getitem__(index)
-        return retval + (index,)
